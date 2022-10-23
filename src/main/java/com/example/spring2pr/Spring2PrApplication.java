2b4342a463127bda4d0070584660defa7ee608b6
KateKolokhina <39592816+KateKolokhina@users.noreply.github.com>
2022-10-23 19:29:00 +0300

    First commit

---
+++ src/main/java/com/example/spring2pr/Spring2PrApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring2pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2PrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring2PrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring2pr/Spring2PrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring2pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2PrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
